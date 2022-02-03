80d7ac849c2d014358fe4383b1c17d34ab9d9bb5
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2022-02-02 16:02:50 -0800

    Fixed image checker 4

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,7 +17,7 @@ public class MarkdownParse {
             if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
                 break;
             }
-            if (!(markdown.charAt(nextCloseBracket - 1) == '!') && nextCloseBracket + 1 == openParen && nextOpenBracket + 1 != nextCloseBracket) {
+            if (!(markdown.charAt(nextOpenBracket - 1) == '!') && nextCloseBracket + 1 == openParen && nextOpenBracket + 1 != nextCloseBracket) {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;
