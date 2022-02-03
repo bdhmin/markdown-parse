6c81a5bb48230125c5a16a315140d6f316ee53d6
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2022-02-02 16:14:19 -0800

    Fixed image checker 4

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,18 +10,17 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int foundExclaim = markdown.indexOf("!", currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if (foundExclaim != -1 || foundExclaim + 1 == nextOpenBracket) {
-                nextOpenBracket = markdown.indexOf("[", foundExclaim);
-            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
                 break;
             }
-            if (foundExclaim == -1 && nextCloseBracket + 1 == openParen && nextOpenBracket + 1 != nextCloseBracket) {
+            if (markdown.charAt(nextCloseBracket - 1) == '!') {
+
+            }
+            else if (nextCloseBracket + 1 == openParen && nextOpenBracket + 1 != nextCloseBracket) {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;
